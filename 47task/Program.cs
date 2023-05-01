cc66d349bd62fc338c7137070b5fb8fa6e55d267
Наталья Сибгатулина <mehryak@mail.ru>
2023-05-02 00:54:08 +0500

    47add

--- 47task/Program.cs
+++ 47task/Program.cs
@@ -4,37 +4,6 @@ m = 3, n = 4.
 0,5 7 -2 -0,2
 1 -3,3 8 -9,9
 8 7,8 -7,1 9 */
-/* double[,] twoDimArray = new double[m, n];
-Random rnd = new Random();
-int[,] Creatarray(int length1, int length2)
-{
-int[,] Array = new int [length1, length2];
-for (int i = 0; i < Array.GetLength(0); i++)
-{
-    for (int j = 0; j < Array.GetLength(1); j++)
-    {
-        //Array[i,j] = new Random(Math.Round.Next)();
-        Array[i,j] = Convert.ToDouble(rnd.Next());
-    }
-}
-return Array;
-
-}
-
-void PrintArray (int [,] Array)
-{
-for (int i = 0; i < Array.GetLength(0); i++)
-{
-    for (int j = 0; j < Array.GetLength(1); j++)
-    {
-        System.Console.Write($"{Array [i,j]}  ");
-
-    }
-    System.Console.WriteLine();
-}
-
-}
-PrintArray(Creatarray(4,4)); */
 
 Console.WriteLine("Задайте количество строк двумерного массива:");
 int m = Convert.ToInt32(Console.ReadLine());
